60ce4dc9795d55f2365d773e044237276a4f01d5
sergio sanhueza <sergio@naitus.cl>
2025-12-01 00:36:12 -0300

    feat: add initial folders and components to work with the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './modules/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./modules/home/home.component').then(m => m.HomeComponent)
+            },
+            {
+                path: 'login',
+                loadComponent: () => import('./modules/login/login.component').then(m => m.LoginComponent)
+            }
+
+        ]
+    }
+];
---
+++ src/app/modules/analysis/analysis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis',
+  imports: [],
+  templateUrl: './analysis.component.html',
+  styleUrl: './analysis.component.scss',
+  standalone: true,
+})
+export class AnalysisComponent {
+
+}
